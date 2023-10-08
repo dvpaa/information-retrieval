f9018e16ab0b941b82dc37887332679c1263bc4c
geonu <kgo000926@gmail.com>
2023-10-09 00:46:41 +0900

    feat: compare analyzers

--- morpheme_analyzer/comparison.py
+++ morpheme_analyzer/comparison.py
@@ -1,11 +1,22 @@
 from konlpy.tag import Hannanum
+from konlpy.tag import Kkma
+from konlpy.tag import Komoran
 
-analyzer = Hannanum()
+
+analyzers = [Hannanum(), Kkma(), Komoran()]
 examples = [
     u'아버지가방에들어가신다',  # 띄어쓰기
-    u'나는 밥을 먹는다', u'하늘을 나는 자동차',  # 중의성 해소
+    u'나는 밥을 먹는다',
+    u'하늘을 나는 자동차',  # 중의성 해소
     u'아이폰 기다리다 지쳐 애플공홈에서 언락폰질러버렸다 6+ 128기가실버ㅋ'
 ]
 
-for example in examples:
-    print(analyzer.analyze(example))
+for analyzer in analyzers:
+    print("--------------------------------------")
+    print(analyzer.__class__.__name__)
+    for example in examples:
+        print("-------------------")
+        print(example)
+        print(analyzer.pos(example))
+        print("-------------------")
+    print("--------------------------------------")
