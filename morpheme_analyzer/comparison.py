7e0ac7641cd5c9d13639c73c42ccff4f54dee308
Geonu Kim <kgo000926@gmail.com>
2023-10-10 18:42:35 +0900

    feat: final compare

--- morpheme_analyzer/comparison.py
+++ morpheme_analyzer/comparison.py
@@ -2,12 +2,19 @@ from konlpy.tag import Hannanum
 from konlpy.tag import Kkma
 from khaiii import KhaiiiApi
 
+# from konlpy.corpus import kolaw
+# c = kolaw.open('constitution.txt').read()
+# print(c)
+
+# api = KhaiiiApi()
+# for word in api.analyze(c):
+#     print(word)
 
 analyzers = [Hannanum(), Kkma(), KhaiiiApi()]
 examples = [
     u'아버지가방에들어가신다',  # 띄어쓰기
     u'사랑해보고싶어',
-    u'하늘을 나는 자동차',  # 중의성 해소
+    u'하늘을 나는 자동차',  # 중의성
 ]
 
 for example in examples:
