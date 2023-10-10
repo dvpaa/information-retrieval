121a347b1ca3a816b1f3449f199950f3afa30b81
Geonu Kim <kgo000926@gmail.com>
2023-10-10 15:36:22 +0900

    feat: compare analyzers

--- morpheme_analyzer/comparison.py
+++ morpheme_analyzer/comparison.py
@@ -1,22 +1,23 @@
 from konlpy.tag import Hannanum
 from konlpy.tag import Kkma
-from konlpy.tag import Komoran
+from khaiii import KhaiiiApi
 
 
-analyzers = [Hannanum(), Kkma(), Komoran()]
+analyzers = [Hannanum(), Kkma(), KhaiiiApi()]
 examples = [
     u'아버지가방에들어가신다',  # 띄어쓰기
-    u'나는 밥을 먹는다',
+    u'사랑해보고싶어',
     u'하늘을 나는 자동차',  # 중의성 해소
-    u'아이폰 기다리다 지쳐 애플공홈에서 언락폰질러버렸다 6+ 128기가실버ㅋ'
 ]
 
 for example in examples:
-    print("--------------------------------------")
     print(example)
     for analyzer in analyzers:
-        print("-------------------")
+        print()
         print(analyzer.__class__.__name__)
-        print(analyzer.pos(example))
-        print("-------------------")
-    print("--------------------------------------")
+        if isinstance(analyzer, KhaiiiApi):
+            print(*analyzer.analyze(example))
+        else:
+            print(analyzer.pos(example))
+    print()
+    print()
